eac44d498bb07e77051155f5b876719b79a3bf05
liyanbo <liyanbo@yiche.com>
2022-11-28 23:37:52 +0800

    update code

--- server/app/router.ts
+++ server/app/router.ts
@@ -2,19 +2,20 @@ import { Application } from 'egg';
 
 export default (app: Application) => {
   const { controller, router } = app;
-  router.get('/api-gateway', controller.home.index)
+  let baseUri = '/api-gateway'
+  router.get(baseUri, controller.home.index)
     // 登录
-    .post('/regist', controller.login.regist)
-    .post('/login', controller.login.login)
-    .get('/reset', controller.login.reset)
-    .post('/users/info', controller.users.info)
+    .post(baseUri + '/regist', controller.login.regist)
+    .post(baseUri + '/login', controller.login.login)
+    .get(baseUri + '/reset', controller.login.reset)
+    .post(baseUri + '/users/info', controller.users.info)
     // 审核
-    .get('/verify/list', controller.verify.list)
-    .get('/verify/verify', controller.verify.verify)
+    .get(baseUri + '/verify/list', controller.verify.list)
+    .get(baseUri + '/verify/verify', controller.verify.verify)
     // 队员信息
-    .get('/member/list', controller.member.list)
-    .post('/member/create', controller.member.insert)
+    .get(baseUri + '/member/list', controller.member.list)
+    .post(baseUri + '/member/create', controller.member.insert)
     //活动
-    .post('/common/uploadPic', controller.common.uploadPic)
-    .post('/activity/create', controller.activity.create)
+    .post(baseUri + '/common/uploadPic', controller.common.uploadPic)
+    .post(baseUri + '/activity/create', controller.activity.create)
 };
